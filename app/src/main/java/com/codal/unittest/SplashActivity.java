8475039bab66693505e4fb308346773e63b77b96
ruchitshah2109 <rshah@codal.com>
2016-05-04 18:57:53 +0530

    Seconda comit with once activity and layout.

---
+++ app/src/main/java/com/codal/unittest/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.codal.unittest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DELL PC on 04-05-2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashactivity);
+    }
+}
