4e3d42b17f93aeff514a2191211640653a3817b6
ruchitshah2109 <rshah@codal.com>
2016-05-04 16:58:50 +0530

    Adding Codal UnitTest Sample inorder to do R&D of Bitrise

---
+++ app/src/test/java/com/codal/unittest/CodalUnitTest.java
@@ -0,0 +1,15 @@
+package com.codal.unittest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class CodalUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
